ce72fb39dc9cc7ca03839a54bfaa5f52340219eb
mrdm026M <mrdm026@gmail.com>
2021-10-09 16:30:34 +0530

    new files

---
+++ 01-Basic/code/02-User_Input.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int num1, num2, result; // variables
+    cout << "Enter number 1: "; 
+    cin >> num1; // user input for first number
+    cout << "Enter number 2: ";
+    cin >> num2; // user input for number 2
+    result = num1 + num2; // add both the numbers
+    cout << "Result : " << result; // printing the result;
+    return 0;
+}
